8c82dde9c253aeed772a0c921c01bb934b66b1ae
arnaud <arnaud.feldmann@gmail.com>
2022-01-04 20:25:45 +0100

    ok

--- src/Cartes/CollectionDeCartes.java
+++ src/Cartes/CollectionDeCartes.java
@@ -2,15 +2,18 @@ package Cartes;
 
 import java.util.ArrayList;
 
+// Cette classe a pour but d'implémenter l'idée d'un "jeu", dont une partie est commune entre joueurs donc m_jeu_pt
+// est un pointeur
+
 public class CollectionDeCartes {
 
     private final ArrayList<Carte> m_main;
     private final ArrayList<Carte> m_jeu_pt;
 
-    public CollectionDeCartes(ArrayList<Carte> main, ArrayList<Carte> reference_jeu) throws IllegalArgumentException {
+    public CollectionDeCartes(ArrayList<Carte> main, ArrayList<Carte> jeu_pt) throws IllegalArgumentException {
         if (main.size() != 2) throw new IllegalArgumentException("Une main a deux cartes");
         m_main = new ArrayList<>(main);
-        m_jeu_pt = reference_jeu;
+        m_jeu_pt = jeu_pt;
     }
 
     public ArrayList<Carte> get_collection() {
@@ -19,7 +22,7 @@ public class CollectionDeCartes {
         return res;
     }
 
-    public ArrayList<Carte> get_reference_jeu() {
+    public ArrayList<Carte> get_jeu_pt() {
         return m_jeu_pt;
     }
 
@@ -41,23 +44,23 @@ public class CollectionDeCartes {
         CollectionDeCartes collection_joueur_ref;
         CollectionDeCartes collection_autre_joueur;
         ArrayList<Carte> main_autre_joueur;
-        ArrayList<Carte> reference_jeu;
+        ArrayList<Carte> jeu_pt;
         boolean victoire;
         int nombre_de_victoires = 0;
         PaquetDeCartes paquet;
         for (int i = 0; i < NB_SIMULATIONS; i++) {
-            reference_jeu = new ArrayList<>(get_reference_jeu());
-            collection_joueur_ref = new CollectionDeCartes(m_main, reference_jeu);
+            jeu_pt = new ArrayList<>(get_jeu_pt());
+            collection_joueur_ref = new CollectionDeCartes(m_main, jeu_pt);
             paquet = new PaquetDeCartes();
             paquet.melanger_cartes();
             paquet.retirer_des_cartes_du_jeu(get_collection());
-            while (reference_jeu.size() != 5) collection_joueur_ref.ajouter_carte_en_jeu(paquet.piocher_une_carte());
+            while (jeu_pt.size() != 5) collection_joueur_ref.ajouter_carte_en_jeu(paquet.piocher_une_carte());
             victoire = true;
             for (int j = 0; j < nombre_autres_joueurs; j++) {
                 main_autre_joueur = new ArrayList<>();
                 main_autre_joueur.add(paquet.piocher_une_carte());
                 main_autre_joueur.add(paquet.piocher_une_carte());
-                collection_autre_joueur = new CollectionDeCartes(main_autre_joueur, reference_jeu);
+                collection_autre_joueur = new CollectionDeCartes(main_autre_joueur, jeu_pt);
                 if (collection_joueur_ref.compareTo(collection_autre_joueur) < 0) victoire = false;
             }
             if (victoire) nombre_de_victoires++;
